53dff6474faf5784d08e3a5f892f86b721509f10
Semih Gur <asemihgur@hotmail.com>
2022-07-30 17:12:19 +0300

    Footer created
    Footer created

---
+++ frontend/components/Footer/Footer.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-footer app padless>
+    <v-container class="blue">
+      <v-row no gutters>
+        <v-col>
+          <v-img
+            src=""
+            class="ml-16 mt-10"
+            cantain
+            max-height="90"
+            max-width="90"
+            size="10 px"
+          ></v-img>
+
+          <p>Anasayfa|Hakkımızda|Vizyon|SSS|</p>
+        </v-col>
+        <v-col>
+          <v-list color="blue">
+            <v-list-item>
+              <v-icon>mdi-map-marker</v-icon>Universiteler Mah. ODTÜ TEKNOKENT,
+              TUSAŞ R&D Building B Block, Çankaya/Ankara
+            </v-list-item>
+            <v-list-item>
+              <v-icon>mdi-email</v-icon>example@hotmail.com
+            </v-list-item>
+            <v-list-item> <v-icon>mdi-phone</v-icon>05435391856 </v-list-item>
+          </v-list>
+        </v-col>
+        <v-col>
+          <h1>Hakkımızda</h1>
+          <v-divider></v-divider>
+          <p class="mt-5">
+            Ülke ve sektörler için öğrenci yetiştirmeyi planlayan ve bu amaç
+            için "Usta-Çırak" uygulamasını kullanan bir firmayız
+          </p>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      icons: ['mdi-facebook', 'mdi-twitter', 'mdi-linkedin', 'mdi-instagram'],
+    }
+  },
+}
+</script>
+
+<style></style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <MentorProfileHeader/>
+  <Footer />
 </template>
 
 <script>
