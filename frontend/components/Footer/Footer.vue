940ddea479779734ee07e014f06531deede95a03
Semih Gur <asemihgur@hotmail.com>
2022-07-31 11:57:46 +0300

    Adjustments to image
    Size and margin adjusted. Image format changed

--- frontend/components/Footer/Footer.vue
+++ frontend/components/Footer/Footer.vue
@@ -4,32 +4,36 @@
       <v-row no gutters>
         <v-col>
           <v-img
-            src="/Logo.jpg"
-            class="ml-24 mt-4 mb-3"
+            src="/Logo.png"
+            class="ml-20 mt-4 mb-3"
             cantain
-            max-height="100"
-            max-width="100"
+            max-height="120"
+            max-width="120"
             size="10 px"
           ></v-img>
 
-          <v-btn text v-for="link in links" :key="link">{{ link }}</v-btn>
+          <v-btn class="ml-4 px-0" text v-for="link in links" :key="link">{{
+            link
+          }}</v-btn>
         </v-col>
         <v-col>
           <v-list color="blue">
             <v-list-item>
-              <v-icon>mdi-map-marker</v-icon>Universiteler Mah. ODTÜ TEKNOKENT,
-              TUSAŞ R&D Building B Block, Çankaya/Ankara
+              <v-icon class="mr-2">mdi-map-marker</v-icon>Universiteler Mah.
+              ODTÜ TEKNOKENT, TUSAŞ R&D Building B Block, Çankaya/Ankara
             </v-list-item>
             <v-list-item>
-              <v-icon>mdi-email</v-icon>example@hotmail.com
+              <v-icon class="mr-2">mdi-email</v-icon>example@hotmail.com
             </v-list-item>
-            <v-list-item> <v-icon>mdi-phone</v-icon>05435391856 </v-list-item>
+            <v-list-item>
+              <v-icon class="mr-2">mdi-phone</v-icon>05435391856
+            </v-list-item>
           </v-list>
         </v-col>
         <v-col>
-          <h1>Hakkımızda</h1>
+          <h1 class="mt-5">Hakkımızda</h1>
           <v-divider></v-divider>
-          <p class="mt-5">
+          <p class="mt-10">
             Ülke ve sektörler için öğrenci yetiştirmeyi planlayan ve bu amaç
             için "Usta-Çırak" uygulamasını kullanan bir firmayız
           </p>
@@ -44,7 +48,7 @@ export default {
   data() {
     return {
       icons: ['mdi-facebook', 'mdi-twitter', 'mdi-linkedin', 'mdi-instagram'],
-      links: ['Hakkımızda', 'Vizyon', 'SSS'],
+      links: ['Vizyon', 'Hakkımızda', 'SSS'],
     }
   },
 }
