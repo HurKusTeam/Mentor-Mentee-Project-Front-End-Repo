3680f5d8c33d5395bc9fa8c1784d0bbe58c3d434
FurkanBerberr <omerfurkanberber20@gmail.com>
2022-07-31 14:48:11 +0300

    Company Profile

---
+++ frontend/components/companyProfile/CompanyProfileCards.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-container class="pt-2 pb-0">
+    <v-card class="mx-auto" max-width="1000" tile>
+      <v-sheet class="mx-auto" elevation="8">
+        <v-slide-group class="pa-4" active-class="success" show-arrows>
+          <v-slide-item v-for="n in 8" :key="n">
+            <v-card class="ma-4" height="250" width="300">
+              <v-list-item>
+                <v-list-item-content style="text-align: center">
+                  <v-list-item-avatar size="100">
+                    <img
+                      src="https://www.w3schools.com/howto/img_avatar.png"
+                      alt="John"
+                    />
+                  </v-list-item-avatar>
+                  <v-list-item-title class="pt-3">
+                    Ömer Furkan Berber
+                  </v-list-item-title>
+                  <v-list-item-subtitle>
+                    Software Engineer
+                  </v-list-item-subtitle>
+                  <v-list-item-subtitle>
+                    <v-icon class="pt-2" size="20"> mdi-email </v-icon>
+                    furkan_berber@gmail.com</v-list-item-subtitle
+                  >
+                  <v-list-item-subtitle>
+                    <v-icon size="20"> mdi-phone </v-icon>
+                    05553216549</v-list-item-subtitle
+                  >
+                </v-list-item-content>
+              </v-list-item>
+            </v-card>
+          </v-slide-item>
+        </v-slide-group>
+      </v-sheet>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.v-application .elevation-8 {
+  box-shadow: none !important;
+}
+</style>
---
+++ frontend/pages/companyProfile.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-main>
+    <CompanyProfileHeader />
+    <CompanyProfileAdvert />
+    <CompanyProfileInfo />
+    <CompanyProfileCards/> 
+  </v-main>
+</template>
+
+<script>
+export default {
+    name: "IndexPage",
+}
+</script>
