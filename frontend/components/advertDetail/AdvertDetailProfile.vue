aa0559a91df8712c6080b464b421e2bdcf6af97a
FurkanBerberr <omerfurkanberber20@gmail.com>
2022-07-31 18:54:08 +0300

    Advert Detail
    Advert detail

---
+++ frontend/components/advertDetail/AdvertDetailProfile.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card style="position: sticky; top: 0;">
+    <v-list-item>
+      <v-list-item-content style="text-align: center">
+        <v-list-item-avatar size="100">
+          <img
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt="John"
+          />
+        </v-list-item-avatar>
+        <v-list-item-title class="pt-3"> Ömer Furkan Berber </v-list-item-title>
+        <v-list-item-subtitle> Software Engineer </v-list-item-subtitle>
+        <v-list-item-subtitle class="pt-2">
+          <v-icon size="20"> mdi-email </v-icon>
+          furkan_berber@gmail.com</v-list-item-subtitle
+        >
+        <v-list-item-subtitle>
+          <v-icon size="20"> mdi-phone </v-icon>
+          05553216549</v-list-item-subtitle
+        >
+      </v-list-item-content>
+    </v-list-item>
+  </v-card>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
