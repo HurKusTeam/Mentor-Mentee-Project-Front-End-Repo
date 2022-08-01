17b0d22d67ad33674415f8235c823da22318e616
Semih Gur <asemihgur@hotmail.com>
2022-08-01 16:36:15 +0300

    Deleted unnecessary files
    Deleted unnecessary files

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -6,9 +6,9 @@
 
 <script>
 import MyApplicant from '../components/MyApplicants.vue/MyApplicant.vue'
-import DenemePage from '../components/MyApplicants.vue/DenemePage.vue'
+
 export default {
   name: 'IndexPage',
-  components: { MyApplicant, DenemePage },
+  components: { MyApplicant },
 }
 </script>
