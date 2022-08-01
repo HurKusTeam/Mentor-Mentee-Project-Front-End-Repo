d34b8335b0247ad7ebfee0ef364421e79492f802
Semih Gur <asemihgur@hotmail.com>
2022-08-01 08:58:06 +0300

    Project opened
    Project opened

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,9 +1,11 @@
 <template>
-  <MentorProfileHeader/>
+  <MyApplicant />
 </template>
 
 <script>
+import MyApplicant from '../components/MyApplicants.vue/MyApplicant.vue'
 export default {
   name: 'IndexPage',
+  components: { MyApplicant },
 }
 </script>
