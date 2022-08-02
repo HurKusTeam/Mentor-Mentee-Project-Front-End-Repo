ed4d7c074c313f2f27fcb4b2400054229a0d6553
Semih Gur <asemihgur@hotmail.com>
2022-08-02 14:14:58 +0300

    Design and function changes
    Integrated to database, profile image made into button, background pattern added

--- frontend/components/MyApplicants.vue/MyApplicant.vue
+++ frontend/components/MyApplicants.vue/MyApplicant.vue
@@ -1,75 +1,82 @@
 <template>
   <v-container>
-    <v-card
-      class="mb-6 mx-auto rounded-lg"
-      max-width="1000"
-      color=""
-      v-for="user in users"
-      :key="user.id"
-    >
-      <v-row>
-        <v-col>
-          <v-card color="" class="pa-2 rounded-lg" outlined tile>
-            <v-list-item class="pa-5">
-              <v-list-item-avatar class="ml-2" size="100">
-                <img
-                  src="https://www.w3schools.com/howto/img_avatar.png"
-                  alt="John"
-                />
-              </v-list-item-avatar>
-              <v-col>
-                <v-list-item-content>
-                  <v-list-item-title class="title">
-                    {{ fullName(user.name, user.surName) }}
-                  </v-list-item-title>
-                  <v-list-item-subtitle>
-                    <v-icon class="pa-1" size="20">mdi-school</v-icon
-                    >{{ user.department }}
-                  </v-list-item-subtitle>
-                  <v-list-item-subtitle>
-                    <v-icon class="pa-1" size="20"> mdi-town-hall </v-icon>
-                    {{ user.uni }}</v-list-item-subtitle
+    <v-flex>
+      <v-card
+        class="mb-6 mx-auto rounded-lg"
+        max-width="1000"
+        color=""
+        v-for="user in users"
+        :key="user.id"
+      >
+        <v-row>
+          <v-col>
+            <v-card class="pa-2 rounded-lg" outlined tile style="border: none">
+              <v-list-item class="pa-5">
+                <v-list-item-avatar class="ml-2" size="100">
+                  <v-btn fab to="/menteeProfile">
+                    <v-img
+                      height="100"
+                      width="100"
+                      src="https://www.w3schools.com/howto/img_avatar.png"
+                      alt="John"
+                  /></v-btn>
+                </v-list-item-avatar>
+                <v-col sm12 md12>
+                  <v-list-item-content>
+                    <v-list-item-title class="title">
+                      {{ fullName(user.UserName, ' ') }}
+                    </v-list-item-title>
+                    <v-list-item-subtitle>
+                      <v-icon class="pa-1" size="20">mdi-school</v-icon
+                      >{{ user.department }}
+                    </v-list-item-subtitle>
+                    <v-list-item-subtitle>
+                      <v-icon class="pa-1" size="20"> mdi-town-hall </v-icon>
+                      {{ user.Universities }}</v-list-item-subtitle
+                    >
+                    <v-list-item-subtitle>
+                      <v-icon class="pa-1" size="20">
+                        mdi-account-school
+                      </v-icon>
+                      {{ user.gpa }}</v-list-item-subtitle
+                    >
+                  </v-list-item-content>
+                </v-col>
+                <v-col sm12 md12 class="ml-4" cols="6">
+                  <v-flex>
+                    <v-icon size="20">mdi-map-marker </v-icon>
+                    {{ user.location }} <v-icon size="20">mdi-linkedin</v-icon>
+                    {{ user.linkedin }} <v-icon size="20">mdi-github</v-icon>
+                    {{ user.gitHub }}
+                  </v-flex>
+                  <v-flex class="mt-2">
+                    <v-icon>mdi-star</v-icon>
+                    <v-list-item-action-subtitle
+                      v-for="skill in user.skills"
+                      :key="skill"
+                    >
+                      {{ skill }},</v-list-item-action-subtitle
+                    >
+                  </v-flex>
+                </v-col>
+                <v-col sm12 md12>
+                  <v-btn
+                    class="mb-3 mt-3"
+                    v-on:click="sendConfirm(user.ID)"
+                    color="green"
                   >
-                  <v-list-item-subtitle>
-                    <v-icon class="pa-1" size="20"> mdi-account-school </v-icon>
-                    {{ user.gpa }}</v-list-item-subtitle
+                    <v-icon>mdi-check</v-icon></v-btn
                   >
-                </v-list-item-content>
-              </v-col>
-              <v-col class="ml-4" cols="6">
-                <v-flex>
-                  <v-icon size="20">mdi-map-marker </v-icon>
-                  {{ user.location }} <v-icon size="20">mdi-linkedin</v-icon>
-                  {{ user.linkedin }} <v-icon size="20">mdi-github</v-icon>
-                  {{ user.gitHub }}
-                </v-flex>
-                <v-flex class="mt-2">
-                  <v-icon>mdi-star</v-icon>
-                  <v-list-item-action-subtitle
-                    v-for="skill in user.skills"
-                    :key="skill"
+                  <v-btn v-on:click="sendDeny(user.ID)" color="red">
+                    <v-icon>mdi-close</v-icon></v-btn
                   >
-                    {{ skill }},</v-list-item-action-subtitle
-                  >
-                </v-flex>
-              </v-col>
-              <v-col>
-                <v-btn
-                  class="mb-3"
-                  v-on:click="sendConfirm(user.id)"
-                  color="green"
-                >
-                  <v-icon>mdi-check</v-icon></v-btn
-                >
-                <v-btn v-on:click="sendDeny(user.id)" color="red">
-                  <v-icon>mdi-close</v-icon></v-btn
-                >
-              </v-col>
-            </v-list-item>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-card>
+                </v-col>
+              </v-list-item>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-flex>
   </v-container>
 </template>
 
@@ -77,37 +84,14 @@
 export default {
   data() {
     return {
+      userDatas: [],
       users: [
         {
           id: 1,
-          name: 'Semih',
+          UserName: 'Semih',
           surName: 'Gür',
           department: 'Yazılım',
-          uni: 'Çankaya Üniversitesi',
-          gpa: 2.54,
-          location: 'Ankara',
-          linkedin: 'semihgur',
-          gitHub: 'github',
-          skills: ['c', 'go'],
-        },
-        {
-          id: 2,
-          name: 'Aust',
-          surName: 'Lawhammer',
-          department: 'Bilgisayar',
-          uni: 'Poşet',
-          gpa: 3.5,
-          location: 'İstanbul',
-          linkedin: 'austlawhammer',
-          gitHub: 'github',
-          skills: ['java', 'c#'],
-        },
-        {
-          id: 1,
-          name: 'Semih',
-          surName: 'Gür',
-          department: 'Yazılım',
-          uni: 'Çankaya Üniversitesi',
+          Universities: 'Çankaya Üniversitesi',
           gpa: 2.54,
           location: 'Ankara',
           linkedin: 'semihgur',
@@ -117,8 +101,17 @@ export default {
       ],
     }
   },
-  computed: {},
+  mounted() {
+    this.getUserData()
+  },
+
   methods: {
+    getUserData() {
+      return this.$axios.$get('/api/GetUsers').then((response) => {
+        this.users = response
+        console.log(response)
+      })
+    },
     sendConfirm(id) {
       return console.log(id)
     },
