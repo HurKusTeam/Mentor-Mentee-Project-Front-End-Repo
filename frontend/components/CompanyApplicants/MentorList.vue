d9ba7a2877b89414f658171db978e2632ec24a38
Semih Gur <asemihgur@hotmail.com>
2022-08-05 19:21:28 +0300

    Update MentorList.vue

--- frontend/components/CompanyApplicants/MentorList.vue
+++ frontend/components/CompanyApplicants/MentorList.vue
@@ -1,77 +1,75 @@
 <template>
   <v-container>
-    <v-flex>
-      <v-card
-        class="mb-6 mx-auto rounded-lg"
-        max-width="1000"
-        color=""
-        v-for="user in users"
-        :key="user.id"
-      >
-        <v-row>
-          <v-col>
-            <v-card class="pa-2 rounded-lg" outlined tile style="border: none">
-              <v-list-item class="pa-5">
-                <v-list-item-avatar class="ml-2" size="100">
-                  <v-img
-                    height="100"
-                    width="100"
-                    src="https://www.w3schools.com/howto/img_avatar.png"
-                    alt="John"
-                  />
-                </v-list-item-avatar>
-                <v-col cols="4">
-                  <v-list-item-content>
-                    <v-list-item-title class="title">
-                      {{ fullName(user.UserName, ' ') }}
-                    </v-list-item-title>
-                    <v-list-item-subtitle>
-                      <v-icon class="pa-1" size="20">mdi-school</v-icon
-                      >{{ user.department }}
-                    </v-list-item-subtitle>
-                    <v-list-item-subtitle>
-                      <v-icon class="pa-1" size="20"> mdi-phone </v-icon>
-                      {{ user.Universities }}</v-list-item-subtitle
-                    >
-                    <v-list-item-subtitle>
-                      <v-icon class="pa-1" size="20"> mdi-email </v-icon>
-                      {{ user.Mail }}</v-list-item-subtitle
-                    >
-                  </v-list-item-content>
-                </v-col>
-                <v-col cols="4" class="ml-4">
-                  <v-flex>
-                    <v-icon size="20">mdi-map-marker </v-icon>
-                    {{ user.location }} <v-icon size="20">mdi-linkedin</v-icon>
-                    {{ user.linkedin }} <v-icon size="20">mdi-github</v-icon>
-                    {{ user.gitHub }}
-                  </v-flex>
-                  <v-flex class="mt-2">
-                    <v-icon>mdi-star</v-icon>
-                    <v-list-item-action-subtitle
-                      v-for="skill in user.skills"
-                      :key="skill"
-                    >
-                      {{ skill }},</v-list-item-action-subtitle
-                    >
-                  </v-flex>
-                </v-col>
-                <v-col>
-                  <v-btn
-                    class="mb-3 mt-3"
-                    v-on:click="sendConfirm(user.ID)"
-                    to="/menteeListPage"
-                    color="green"
+    <v-card
+      class="mb-6 mx-auto rounded-lg"
+      max-width="1000"
+      color=""
+      v-for="user in users"
+      :key="user.id"
+    >
+      <v-row>
+        <v-col>
+          <v-card class="pa-2 rounded-lg" outlined tile style="border: none">
+            <v-list-item class="pa-5">
+              <v-list-item-avatar class="ml-2" size="100">
+                <v-img
+                  height="100"
+                  width="100"
+                  src="https://www.w3schools.com/howto/img_avatar.png"
+                  alt="John"
+                />
+              </v-list-item-avatar>
+              <v-col cols="4">
+                <v-list-item-content>
+                  <v-list-item-title class="title">
+                    {{ fullName(user, ' ') }}
+                  </v-list-item-title>
+                  <v-list-item-subtitle>
+                    <v-icon class="pa-1" size="20">mdi-school</v-icon
+                    >{{ user.department }}
+                  </v-list-item-subtitle>
+                  <v-list-item-subtitle>
+                    <v-icon class="pa-1" size="20"> mdi-phone </v-icon>
+                    {{ user }}</v-list-item-subtitle
                   >
-                    <v-icon>mdi-clipboard-account</v-icon>
-                  </v-btn>
-                </v-col>
-              </v-list-item>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-card>
-    </v-flex>
+                  <v-list-item-subtitle>
+                    <v-icon class="pa-1" size="20"> mdi-email </v-icon>
+                    {{ user }}</v-list-item-subtitle
+                  >
+                </v-list-item-content>
+              </v-col>
+              <v-col cols="4" class="ml-4">
+                <v-flex>
+                  <v-icon size="20">mdi-map-marker </v-icon>
+                  {{ user.location }} <v-icon size="20">mdi-linkedin</v-icon>
+                  {{ user.linkedin }} <v-icon size="20">mdi-github</v-icon>
+                  {{ user.gitHub }}
+                </v-flex>
+                <v-flex class="mt-2">
+                  <v-icon>mdi-star</v-icon>
+                  <v-list-item-action-subtitle
+                    v-for="skill in user.skills"
+                    :key="skill"
+                  >
+                    {{ skill }},</v-list-item-action-subtitle
+                  >
+                </v-flex>
+              </v-col>
+              <v-col>
+                <v-btn
+                  class="mb-3 mt-3"
+                  v-on:click="sendConfirm(user.ID)"
+                  to="/menteeListPage"
+                  color="green"
+                >
+                  <v-icon>mdi-clipboard-account</v-icon>
+                </v-btn>
+              </v-col>
+            </v-list-item>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-card>
   </v-container>
 </template>
 
@@ -97,7 +95,7 @@ export default {
 
   methods: {
     getUserData() {
-      return this.$axios.$get('/api/GetUsers').then((response) => {
+      return this.$axios.$get('/api/MyEmployees').then((response) => {
         this.users = response
         console.log(response)
       })
