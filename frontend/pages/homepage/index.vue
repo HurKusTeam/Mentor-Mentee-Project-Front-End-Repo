9630a3285dbadd13aac53703fcb5eeb5480f3e46
ERBEK06 <osmanbahadirerbek@gmail.com>
2022-08-05 10:31:09 +0300

    Update index.vue

--- frontend/pages/homepage/index.vue
+++ frontend/pages/homepage/index.vue
@@ -1,15 +1,33 @@
 <template>
   <v-main>
     <div >
-    <v-row>
-    <v-col sm="12" md="12" class="pr-1"><header1/></v-col>
-    </v-row>
+    <v-col>
+    <div  >
+  <v-card class="mx-auto hover:bg-cyan-600 " max-width="2000" >
+       <v-btn @click = "show = true"
+      class="ma-8 absolute bottom-0 left-3 ..."
+      outlined
+      color="indigo"
+    >
+      Outlined Button
+       </v-btn>
+    <v-btn @click = "show = false"
+      class="ma-8 absolute bottom-0 left-7 ..."
+      outlined
+      color="indigo"
+    >
+      Outlined Button
+    </v-btn>
+   </v-card>
+</div>
+<hr/>
+    </v-col>
   </div>
     <div class="pt-2 pb-0">
       <v-card color="transparent" class="mx-auto" max-width="2000" flat tile style="border: none">
         <v-row>
-          <v-col sm="6" md="6" class="pr-1"><mentorads/></v-col>
-         <v-col sm="6" md="6" class="pl-1"><company/></v-col>
+          <v-col v-if="show" sm="12" md="12" class="pr-1"><mentorads/></v-col>
+          <v-col v-if="!show" sm="12" md="12" class="pl-1"><company/></v-col>
         </v-row>
       </v-card>
     </div>
@@ -18,5 +36,13 @@
 
 <script>
 export default {
+ 
+  data() {
+    return {
+      show: false,
+    }
+  },
+  
+  
 }
 </script>
