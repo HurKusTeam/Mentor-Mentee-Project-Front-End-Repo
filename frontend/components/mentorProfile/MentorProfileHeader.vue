2064aa1c1cf9c41fa9ea24e0fa0fe16efd577f57
FurkanBerberr <omerfurkanberber20@gmail.com>
2022-08-07 19:29:55 +0300

    Edit ment profile

--- frontend/components/mentorProfile/MentorProfileHeader.vue
+++ frontend/components/mentorProfile/MentorProfileHeader.vue
@@ -10,10 +10,7 @@
         <v-col cols="12" sm="10" md="10">
           <v-list-item class="pa-5">
             <v-list-item-avatar class="ml-2" size="100">
-              <img
-                src="https://www.w3schools.com/howto/img_avatar.png"
-                alt="John"
-              />
+              <img v-if="profilePhoto" :src="`${profilePhoto}`" alt="John" />
             </v-list-item-avatar>
             <v-list-item-content>
               <v-col>
@@ -63,155 +60,14 @@
             <v-btn icon :href="`https://${website}`"
               ><v-icon class="pa-1" size="20"> mdi-web </v-icon></v-btn
             >
-
-            <v-dialog v-model="dialog" persistent max-width="600px">
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn icon color="primary" dark v-bind="attrs" v-on="on">
-                  <v-icon class="pa-1" size="20"> mdi-pencil </v-icon>
-                </v-btn>
-              </template>
-              <v-card>
-                <v-card-title>
-                  <span class="text-h5">Profil Düzenleme</span>
-                </v-card-title>
-                <v-card-text>
-                  <v-container>
-                    <v-row>
-                      <v-col cols="12" sm="6" md="4">
-                        <v-text-field label="Adı" required></v-text-field>
-                      </v-col>
-                      <v-col cols="12" sm="6" md="4">
-                        <v-text-field label="Soyadı"></v-text-field>
-                      </v-col>
-                      <v-col cols="12" sm="6" md="4">
-                        <v-text-field
-                          label="Branş"
-                          hint="Bilgisayar Mühendisi"
-                          required
-                        ></v-text-field>
-                      </v-col>
-                      <v-col cols="12" sm="6">
-                        <v-autocomplete
-                          :items="[
-                            'Hacettepe Üniversitesi ',
-                            'Orta Doğu Teknik Üniversitesi (ODTÜ)',
-                            'Koç Üniversitesi',
-                            'stanbul Teknik Üniversitesi (İTÜ)',
-                            'İstanbul Üniversitesi',
-                            'Ankara Üniversitesi',
-                            'Gazi Üniversitesi',
-                            'Ege Üniversitesi',
-                            'Sabancı Üniversitesi',
-                            'Yıldız Teknik Üniversitesi',
-                            'Boğaziçi Üniversitesi',
-                          ]"
-                          label="Okulu"
-                        ></v-autocomplete>
-                      </v-col>
-                      <v-col cols="12" sm="6">
-                        <v-text-field
-                          label="Doğum Tarihi"
-                          type="date"
-                        ></v-text-field>
-                      </v-col>
-                      <v-col cols="12">
-                        <v-text-field
-                          label="Email*"
-                          hint="örnek_mail@gmail.com"
-                          required
-                        ></v-text-field>
-                      </v-col>
-                      <v-col cols="12">
-                        <v-text-field
-                          label="Password*"
-                          type="password"
-                          required
-                        ></v-text-field>
-                      </v-col>
-                      <v-col cols="12" sm="3">
-                        <v-text-field
-                          label="Linkedin"
-                          hint="linkedin.com"
-                          required
-                        ></v-text-field>
-                      </v-col>
-                      <v-col cols="12" sm="2">
-                        <v-text-field
-                          label="Github"
-                          hint="github.com"
-                          required
-                        ></v-text-field>
-                      </v-col>
-                      <v-col cols="12" sm="3">
-                        <v-text-field
-                          label="Facebook"
-                          hint="facebook.com"
-                          required
-                        ></v-text-field>
-                      </v-col>
-                      <v-col cols="12" sm="2">
-                        <v-text-field
-                          label="Twitter"
-                          hint="twitter.com"
-                          required
-                        ></v-text-field>
-                      </v-col>
-                      <v-col cols="12" sm="2">
-                        <v-text-field
-                          label="Website"
-                          hint="örnek.com"
-                          required
-                        ></v-text-field>
-                      </v-col>
-                      <v-col cols="12" sm="6">
-                        <v-autocomplete
-                          :items="[
-                            'Türkçe',
-                            'İngilizce',
-                            'Çince',
-                            'Almanca',
-                            'Fransızca',
-                            'İspanyolca',
-                            'Hintçe',
-                            'Arapça',
-                          ]"
-                          label="Dil"
-                          multiple
-                        ></v-autocomplete>
-                      </v-col>
-                      <v-col cols="12" sm="6">
-                        <v-autocomplete
-                          :items="[
-                            'Java',
-                            'C#',
-                            'C++',
-                            'Go',
-                            'Vue',
-                            'Python',
-                            'Php',
-                            'JavaScript',
-                            'R',
-                            'Flutter',
-                          ]"
-                          label="Yetenekler"
-                          multiple
-                        ></v-autocomplete>
-                      </v-col>
-                    </v-row>
-                  </v-container>
-                  <small>*Doldurulması zorunlu alanlar</small>
-                </v-card-text>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="blue darken-1" text @click="dialog = false">
-                    Kapat
-                  </v-btn>
-                  <v-btn color="blue darken-1" text @click="dialog = false">
-                    Kaydet
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
+            <v-file-input
+              hide-input
+              type="file"
+              @change="updatePhoto"
+              prepend-icon="mdi-image-edit"
+              class="ma-0 pa-0 pl-1"
+            ></v-file-input>
+            <MentorProfileEdit :company_id="companyId" />
           </v-layout>
         </v-col>
       </v-row>
@@ -223,7 +79,7 @@
 export default {
   data() {
     return {
-      dialog: false,
+      photo: 'https://www.w3schools.com/howto/img_avatar.png',
     }
   },
   props: [
@@ -240,7 +96,26 @@ export default {
     'facebook',
     'twitter',
     'website',
+    'companyId',
+    'profilePhoto'
   ],
+  methods: {
+    updatePhoto(file) {
+      return new Promise((resolve) => {
+        const reader = new FileReader()
+        if (file) {
+          reader.readAsDataURL(file)
+        }
+        reader.onload = async () => {
+          resolve(this.photo = reader.result)
+          let p = {
+              Image: this.photo,
+          }
+          await this.$axios.$post('/api/ProfileImg/', p)
+        }
+      })
+    },
+  },
 }
 </script>
 
