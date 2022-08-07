2621c3d369469a6cf6e5572c22a392925539630f
ERBEK06 <osmanbahadirerbek@gmail.com>
2022-08-07 22:23:46 +0300

    homepageforvisitor

---
+++ frontend/components/homepageforvisitor/company.vue
@@ -0,0 +1,80 @@
+<template>
+    <v-container>
+        <v-card class="mx-auto mb-2 rounded-lg" max-width="1500" tile v-for="user in company1" :key="user.id1">
+            <v-row>
+                <v-col sm="4" md="4" class="pr-1">
+                    <v-list-item>
+                        <v-list-item-content>
+                            <v-list-item-avatar size="100" left="true">
+                                <img src="https://i.pinimg.com/originals/99/7b/e0/997be08fea707cddda41d010e04d6a3e.png"
+                                    alt="Aselsan" />
+                            </v-list-item-avatar>
+                            <v-list-item-title class="pt-3"> ASELSAN </v-list-item-title>
+                            <v-list-item-subtitle> {{ user.Company.Sector }} </v-list-item-subtitle>
+                            <v-list-item-subtitle class="pt-2">
+                                <v-icon size="20"> mdi-email </v-icon>
+
+                                aselsan_ik@gmail.com
+                            </v-list-item-subtitle>
+                            <v-list-item-subtitle>
+                                <v-icon size="20"> mdi-phone </v-icon>
+
+                                {{ user.Advert.CompanyID }}
+                            </v-list-item-subtitle>
+                        </v-list-item-content>
+                    </v-list-item>
+
+                </v-col>
+
+                <v-col sm="8" md="8" class="pl-1">
+                    <v-list-item>
+                        <v-list-item-content>
+                            <v-list-item-title class="pt-3" ma-8> İLAN BİLGİLERİ </v-list-item-title>
+                            <v-list-item-subtitle> {{user.Advert.Description}}
+                            </v-list-item-subtitle>
+                        </v-list-item-content>
+                    </v-list-item>
+
+
+                </v-col>
+
+            </v-row>
+
+        </v-card>
+
+
+    </v-container>
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            company1: [],
+
+
+        }
+
+    },
+
+    methods: {
+        async company() {
+            return await this.$axios.$get('/api/GetAdvertsAll').then((response) => {
+                this.company1 = response
+
+                console.log(response)
+            })
+        },
+    },
+
+    mounted() {
+        this.company()
+    },
+
+
+}
+</script>
+
+<style>
+</style>
---
+++ frontend/components/homepageforvisitor/indmentors.vue
@@ -0,0 +1,113 @@
+<template>
+  <v-container class="pb-0">
+    <v-card class="mx-auto mb-5 rounded-lg" max-width="1500" tile
+       v-for="user in users"
+       :key="user.id1"
+    >
+      <v-img
+        height="200"
+        src="https://c4.wallpaperflare.com/wallpaper/500/442/354/outrun-vaporwave-hd-wallpaper-preview.jpg"
+      ></v-img>
+
+      <v-row no-gutters>
+        <v-col cols="12" sm="10" md="10">
+          <v-list-item class="pa-5">
+            <v-list-item-avatar class="ml-2" size="100">
+              <img
+                src="https://www.w3schools.com/howto/img_avatar.png"
+                alt="John"
+              />
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-col>
+                <v-list-item-title class="title">
+                  {{ user.Name }} {{ user.Surname }}
+                  <v-list-item-subtitle> {{user.Major}} </v-list-item-subtitle>
+                </v-list-item-title>
+               
+                  
+                <v-list-item-subtitle>
+                  <v-icon class="pa-1" size="20"> mdi-school </v-icon>
+                  {{ user.Uni }}
+                </v-list-item-subtitle>
+
+
+
+                <v-list-item-subtitle>
+                  <v-icon class="pa-1" size="20"> mdi-calendar-range </v-icon>
+                  {{ user.Birthdate }}
+                </v-list-item-subtitle> </v-col
+              ><v-col>
+                <v-list-item-subtitle>
+                  <v-icon class="pa-1" size="20"> mdi-email </v-icon>
+                  {{ user.Mail }}
+                </v-list-item-subtitle>
+                <v-list-item-subtitle>
+                  <v-icon class="pa-1" size="20"> mdi-phone </v-icon>
+                  {{ user.Tel }}</v-list-item-subtitle
+                >
+                <v-list-item-subtitle>
+                  <v-icon class="pa-1" size="20"> mdi-map-marker </v-icon>
+                  {{ user.City }}
+                </v-list-item-subtitle></v-col
+              >
+            </v-list-item-content>
+          </v-list-item>
+        </v-col>
+        <v-col cols="6" sm="2" md="2">
+          <v-layout class="pa-3" justify-end>
+            <v-btn icon :href="`https://${user.Linkedin}`"
+              ><v-icon class="pa-1" size="20"> mdi-linkedin </v-icon></v-btn
+            >
+            <v-btn icon :href="`https://${user.Github}`"
+              ><v-icon class="pa-1" size="20"> mdi-github </v-icon></v-btn
+            >
+            <v-btn icon :href="`https://${user.Facebook}`"
+              ><v-icon class="pa-1" size="20"> mdi-facebook </v-icon></v-btn
+            >
+            <v-btn icon :href="`https://${user.Twitter}`"
+              ><v-icon class="pa-1" size="20"> mdi-twitter </v-icon></v-btn
+            >
+            <v-btn icon :href="`https://${user.Website}`"
+              ><v-icon class="pa-1" size="20"> mdi-web </v-icon></v-btn
+            >
+
+            
+          </v-layout>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+data() {
+    return {
+      users: [],
+      users1: [],
+      
+    }
+
+  },
+
+  methods: {
+    async createUser() {
+      return await this.$axios.$get('/api/IndividualMentors').then((response) => {
+        this.users = response
+        
+        console.log(response)
+      })
+    },
+  },
+
+  mounted() {
+    this.createUser()
+  },
+
+}
+</script>
+
+<style>
+
+</style>
